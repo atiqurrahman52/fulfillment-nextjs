9c864c3ead67427fdfd13d8969fbcd2108814041
atiqurrahman52 <atiqur.rahman@intelli.global>
2022-12-01 15:10:56 +0600

    faq page updated

---
+++ pages/faq.js
@@ -0,0 +1,14 @@
+import React from "react";
+import FaqBanner from "../components/faq/FaqBanner";
+import FaqDetails from "../components/faq/FaqDetails";
+
+const Faq = () => {
+  return (
+    <div>
+      <FaqBanner />
+      <FaqDetails />
+    </div>
+  );
+};
+
+export default Faq;
