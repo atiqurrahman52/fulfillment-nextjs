1900a669b233d2e07f6d386cbb80c17f4932df8c
atiqurrahman52 <atiqur.rahman@intelli.global>
2022-12-01 11:16:19 +0600

    layout components updated

--- layouts/BasicLayout.js
+++ layouts/BasicLayout.js
@@ -1,12 +1,15 @@
 
 
+import Head from "next/head";
 import Footer from "../components/navigation/Footer";
 import NavBar from "../components/navigation/Navbar";
 
 const BasicLayout = ({children, head}) => {
   return (
     <>
-
+        <Head>
+          <title>{head? head: 'Packme'}</title>
+        </Head>
       <NavBar />
          {children}
       <Footer />
